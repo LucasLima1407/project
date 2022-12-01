3b0715ae39afb4c7770a66b42d234ae5c3e24898
mardson666 <mardson49@gmail.com>
2022-11-30 21:32:40 -0300

    commit 4
    teste de commit

---
+++ project/project v2/project/target/classes/db/migration/V1__Create_Table_Admin.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS `admin` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nome` varchar(50) NOT NULL DEFAULT '0',
+  `login` varchar(50) DEFAULT NULL,
+  `senha` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) 
+
+
---
+++ project/project v2/project/target/classes/db/migration/V2__Populate_Table_Admin.sql
@@ -0,0 +1,2 @@
+INSERT INTO `admin` (`id`, `nome`, `login`, `senha`) VALUES
+	(1, 'mardson\r\n', 'root', 'root');
